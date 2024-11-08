e9b91127a8e83a56f09ac209928e20877defb1db
Fabrício Sousa <151534686+fabriciosousa221@users.noreply.github.com>
2024-11-08 16:51:41 -0300

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,12 @@
+if (true){
+    alert("JavaScript eh a melhor linguagem de programacao");
+   }
+else{
+    alert("Odeio JavaScript");
+   }
+
+   if (false){
+    alert("Eu adoro ouvir Pablo Vittar");
+   }else{
+    alert("Adoro ouvir Iron Maiden");
+   }
